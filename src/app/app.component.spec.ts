9107a3b38332273f14ae6010279f07478cfb9882
Dani Aguilar <47434434+D4niel0@users.noreply.github.com>
2024-11-17 19:43:16 +0100

    Initial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-base-project' title`, () => {
+  it(`should have the 'dynamic-modals-ngzorro' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-base-project');
+    expect(app.title).toEqual('dynamic-modals-ngzorro');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-base-project');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, dynamic-modals-ngzorro');
   });
 });
